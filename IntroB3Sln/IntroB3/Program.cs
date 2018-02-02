e0ee8e75759e669f0a6e049c110edd376adc6a3c
Jahid Hassan <Jahid Hassan@DESKTOP-DMC0B0D>
2018-02-02 10:51:09 +0600

    Initialize

---
+++ IntroB3Sln/IntroB3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroB3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
